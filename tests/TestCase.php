5b347b4c68cb8ad0324ab78eb4e9f524291e82a0
Nils Refle <apfelfrisch@gmail.com>
2021-03-23 09:43:09 +0100

    feat: Support PHP8

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,6 @@
 
 namespace Proengeno\ReadingCalculator\Test;
 
-use Mockery as m;
-
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
-    protected function tearDown(): void
-    {
-        m::close();
-    }
 }
